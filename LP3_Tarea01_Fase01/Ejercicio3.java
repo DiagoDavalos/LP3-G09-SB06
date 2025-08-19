7fa44f64a03ca1579845dfd56a28b4c1215f9ecf
DiagoDavalos <diagoeduardo009@gmail.com>
2025-08-19 00:48:21 -0500

    organizando ejercicios en carpeta LP3_Tarea01_Fase01

---
+++ LP3_Tarea01_Fase01/Ejercicio3.java
@@ -0,0 +1,23 @@
+package PrimeroT1;
+
+import java.util.Random;
+
+public class Ejercicio3 {
+    public static void main(String[] args) {
+        Random rand = new Random();
+        int[] frecuencia = new int[6];
+
+        // 20 000 lanzamientos
+        for (int i = 0; i < 20000; i++) {
+            int lanzamiento = rand.nextInt(6) + 1; // genera un número entre 1 y 6
+
+            // aumentar la frecuencia según la cara
+            frecuencia[lanzamiento - 1]++;
+        }
+
+        // Mostrar resultados
+        for (int i = 0; i < 6; i++) {
+            System.out.println("Cara " + (i+1) + ": " + frecuencia[i] + " veces");
+        }
+    }
+}
